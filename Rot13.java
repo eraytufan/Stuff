561fca30fb163d342b9be7d49d979be4e3b1f462
eraytufan <32275504+eraytufan@users.noreply.github.com>
2018-04-25 18:16:45 +0300

    Add files via upload

---
+++ Rot13.java
@@ -0,0 +1,36 @@
+
+
+public class Rot13 {
+	public static void main(String[] args) {
+        
+		String str = "Shingeki no Kyojin";
+		 
+        System.out.println(encode(str, 13));
+        System.out.println(decode(encode(str, 13), 13));
+		
+    }   
+	public static String decode(String text, int number) {
+		return encode(text, 26-number);
+	}
+	public static String encode(String text, int number) {
+		int offset = number % 26;
+		
+		String string = "";
+		for (int i = 0; i < text.length(); i++) {
+			if (!Character.isLetter(text.charAt(i))) {
+				string += text.charAt(i);
+				continue;
+			}
+			if (Character.isUpperCase(text.charAt(i))) {
+				string += (char) ((int) ('A' + (text.charAt(i) + offset - 'A') % 26 ));
+			}
+			else {
+				string += (char) ((int) ('a' + (text.charAt(i) + offset - 'a') % 26 ));
+			}
+			
+		}
+		
+		
+		return string;
+	}
+}
